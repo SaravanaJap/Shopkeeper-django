9adbd70fb7661a947ec7898d1873f695c01f1b93
SaravanaJap <saravanajagan607@gmail.com>
2025-01-20 22:53:36 +0530

    django authentication

--- Shopkeeper/settings.py
+++ Shopkeeper/settings.py
@@ -132,7 +132,22 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger',
+    
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#smtp configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'saravanajagan607@gmail.com'
+EMAIL_HOST_PASSWORD = 'nwashoibtrhxqred'
+EMAIL_USE_TLS = True
